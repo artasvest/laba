0ad06904a31c20139be02c8921498a7830f96cb8
artasvest <uragar2@gmail.com>
2024-11-14 22:48:58 +0300

    final

---
+++ pit3.py
@@ -0,0 +1,49 @@
+def int_input():
+    while True:
+        try:
+            n = int(input("Введите длину списка: "))
+            return n
+        except ValueError:
+            print("Ошибка: длина списка должна быть целым числом")
+
+
+import random
+
+n = int_input()
+min_value = -100
+max_value = 100
+
+
+
+minus1 = 0
+minus2= 0
+summamin = 0
+
+summanech = 0
+
+spisok = [random.randint(min_value,max_value) for _ in range(n)]
+
+for i in range(len(spisok)):
+    if spisok[i]<0:
+        minus1 = i
+        break
+
+for i in range(minus1,len(spisok)):
+    if spisok[i]<0:
+        minus2= i
+
+for i in range(minus1+1,minus2):
+    summamin += spisok[i]
+
+for i in range(len(spisok)):
+    if i%2!=0:
+        summanech += spisok[i]
+
+
+
+
+print("Сам список: ",spisok)
+
+print("Сумма элементов списка с нечетными номерами равна: ",summanech)
+print("Сумма элементов списка, расположенных между первым и последним отрицательными элементами равна: ",summamin)
+
---
+++ piton2.py
@@ -0,0 +1,61 @@
+def is_latin(word):
+    for char in word:
+        if not ('a' <= char <= 'z' or 'A' <= char <= 'Z'):
+            return False
+    return True
+
+def custom_append(lst, item):
+    lst += [item]
+    return lst
+
+def split_text(text):
+    words = []
+    word = ''
+    for char in text:
+        if char == ' ':
+            if word:
+                words = custom_append(words, word)
+                word = ''
+        else:
+            word += char
+    if word: 
+        words = custom_append(words, word)
+    return words
+
+def join_words(words, separator=' '):
+    result = ''
+    for i in range(len(words)):
+        result += words[i]
+        if i < len(words) - 1:
+            result += separator
+    return result
+
+def custom_all(iterable):
+    for element in iterable:
+        if not element:
+            return False
+    return True
+
+def filter_words(text):
+    words = split_text(text)  
+    if not custom_all(is_latin(word) for word in words): 
+        return "Ошибка: Текст должен содержать только латинские буквы!"
+
+    if len(words) < 2 or len(words) > 30:  
+        return "Ошибка: В тексте должно быть от 2 до 30 слов."
+
+    if not custom_all(2 <= len(word) <= 10 for word in words):
+        return "Ошибка: Каждое слово должно содержать от 2 до 10 букв."
+
+    last_word = words[-1]
+    filtered_words = []
+
+    for word in words[:-1]: 
+        if word != last_word:
+            filtered_words = custom_append(filtered_words, word) 
+
+    return join_words(filtered_words)
+
+text = str(input("Введите текст на латинице: "))
+result = filter_words(text)
+print(result)  
